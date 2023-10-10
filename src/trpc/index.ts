ba88c84af1719ea69cac2dc40db1842e85ef8653
Igor Alyanoy <igoralyanoy@gmail.com>
2023-10-11 01:03:19 +0300

    feat: add prisma

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,33 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
 import { publicProcedure, router } from './trpc';
 
 export const appRouter = router({
   greeting: publicProcedure.query(() => 'hello tRPC v10!'),
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user.id || !user.email) {
+      throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+
+    const findUser = await db.user.findFirst({
+      where: { id: user.id },
+    });
+
+    if (!findUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+
+    return { success: true };
+  }),
 });
 
 export type AppRouter = typeof appRouter;
