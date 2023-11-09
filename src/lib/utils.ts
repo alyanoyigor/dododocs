2fd63b0675569c35d9dcd9ac14451711b7bfb09c
Igor Alyanoy <igoralyanoy@gmail.com>
2023-11-09 23:09:04 +0200

    feat: add get absolute url func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,15 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const getAsoluteUrl = (path: string) => {
+  if (typeof window !== 'undefined') {
+    return path;
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}${path}`;
+  }
+
+  return `https://localhost:${process.env.PORT ?? 3000}`;
+};
