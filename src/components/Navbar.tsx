c7c5f4f2638dcaf4dcd574708c3a5fd9abaae140
Igor Alyanoy <igoralyanoy@gmail.com>
2023-11-13 23:12:31 +0200

    feat: add auth condition in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,12 @@ import MaxWidthWrapper from './MaxWidthWrapper';
 import { buttonVariants } from './ui/button';
 import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
 import { ArrowRight } from 'lucide-react';
+import { getKindeUser } from '@/lib/auth';
+import NavbarUserAccount from './NavbarUserAccount';
 
 const Navbar = () => {
+  const user = getKindeUser();
+
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -16,32 +20,56 @@ const Navbar = () => {
           {/* todo: Add mobile navbar */}
 
           <div className="items-center space-x-4 sm:flex ">
-            <>
-              <Link
-                className={buttonVariants({
-                  variant: 'ghost',
-                  size: 'sm',
-                })}
-                href="/pricing"
-              >
-                Pricing
-              </Link>
-              <LoginLink
-                className={buttonVariants({
-                  variant: 'ghost',
-                  size: 'sm',
-                })}
-              >
-                Sign in
-              </LoginLink>
-              <RegisterLink
-                className={buttonVariants({
-                  size: 'sm',
-                })}
-              >
-                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
-              </RegisterLink>
-            </>
+            {!user && (
+              <>
+                <Link
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}
+                  href="/pricing"
+                >
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}
+                >
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: 'sm',
+                  })}
+                >
+                  Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+                </RegisterLink>
+              </>
+            )}
+            {user && (
+              <>
+                <Link
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}
+                  href="/dashboard"
+                >
+                  Dashboard
+                </Link>
+                <NavbarUserAccount
+                  name={
+                    user.given_name && user.family_name
+                      ? `${user.given_name} ${user.family_name}`
+                      : 'Your account'
+                  }
+                  email={user.email ?? undefined}
+                  imageUrl={user.picture ?? ''}
+                />
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
---
+++ src/components/NavbarUserAccount.tsx
@@ -0,0 +1,61 @@
+import Image from 'next/image';
+import { Avatar, AvatarFallback } from './ui/avatar';
+import { Button } from './ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import Icons from './Icons';
+
+type NavbarUserAccountProps = {
+  name: string;
+  imageUrl: string;
+  email?: string;
+};
+
+const NavbarUserAccount = ({
+  email,
+  imageUrl,
+  name,
+}: NavbarUserAccountProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="overflow-visible">
+        <Button className="rounded-full h-8 w-8 aspect-square bg-slate-400">
+          <Avatar className="relative h-8 w-8">
+            {imageUrl ? (
+              <div className="relative aspect-square h-full w-full">
+                <Image
+                  fill
+                  src={imageUrl}
+                  alt="Profile picture"
+                  referrerPolicy="no-referrer"
+                />
+              </div>
+            ) : (
+              <AvatarFallback>
+                <span className="sr-only">{name}</span>
+                <Icons.user className="h-4 w-4 text-zinc-900" />
+              </AvatarFallback>
+            )}
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-0.5 leading-none">
+            {name && <p className="font-medium text-sm text-black">{name}</p>}
+            {email && (
+              <p className="w-[200px] truncate text-xs text-zinc-700">
+                {email}
+              </p>
+            )}
+          </div>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default NavbarUserAccount;
