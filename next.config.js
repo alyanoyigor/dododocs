a95ec6ec048f03e7601940ecb8afac838550451f
Igor Alyanoy <igoralyanoy@gmail.com>
2024-11-14 22:08:55 +0200

    feat: add prettier, add not-found page

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import React from 'react';
+
+const PageNotFound = () => {
+  return (
+    <MaxWidthWrapper className="flex justify-center mt-20">
+      <h1 className="text-4xl font-bold">404</h1>
+    </MaxWidthWrapper>
+  );
+};
+
+export default PageNotFound;
